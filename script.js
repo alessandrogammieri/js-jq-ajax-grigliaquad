b53778065e9ee83283dab3be17b195968754fc4c
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-29 16:04:49 +0200

    ajax + api

--- script.js
+++ script.js
@@ -6,7 +6,28 @@ Il numero ottenuto appare al centro del quadrato */
 
 $( document ).ready(function () {
 
-
-
+  // Ad ogni click parte una richiesta Ajax
+  $(".quadro").one("click", function () {
+    var cliccato = $(this);
+    // Richiediamo via ajax all'API un numero random
+    $.ajax({
+      url : "https://flynn.boolean.careers/exercises/api/random/int",
+      method: "GET",
+      success: function (data) {
+        var numapi = data.response;
+        // Verifichiamo se il numero è > o < di 5
+        if (numapi <= 5) {
+          $(cliccato).addClass("yellow");
+          $(cliccato).text(numapi);
+        } else if (numapi > 5) {
+          $(cliccato).addClass("green");
+          $(cliccato).text(numapi);
+        }
+      },
+      error: function (errore) {
+        alert ("C'è stato un errore: " + errore);
+      }
+    });
+  });
 
 });
